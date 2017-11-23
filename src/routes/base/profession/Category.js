22fdebd1d67e640b6311df2c07c4f058f34b3e4c
cherrymeteor <cherrymeteor@outlook.com>
2017-11-23 20:23:03 +0800

    fix

--- src/routes/base/profession/Category.js
+++ src/routes/base/profession/Category.js
@@ -1,95 +1,66 @@
 import React, {Component} from 'react';
-import {Form, Tabs} from 'antd';
-
-const TabPane = Tabs.TabPane;
+import {Form, Pagination, Table} from 'antd';
+import {loadDataProfessionCategoryDataSet} from "../../../service/base";
 
 const table_columns = [
   {title: '序号', dataIndex: 'id', key: 'id'},
-  {title: '专业名称', dataIndex: 'profession', key: 'profession'},
-  {title: '专业代码', dataIndex: 'professionCode', key: 'professionCode'},
-  {title: '所属学科', dataIndex: 'subjectName', key: 'subjectName'},
-  {title: '所属门类', dataIndex: 'categoryName', key: 'categoryName'},
-  {title: '修业年限', dataIndex: 'revisedYears', key: 'revisedYears'},
-  {title: '授予学位', dataIndex: 'degree', key: 'degree'},
-  {title: '毕业5年', dataIndex: 'salary', key: 'salary'},
-  {
-    title: '开设院校', dataIndex: 'offer', key: 'offer', render: (text) => {
-    text = text.replace(/<.+\/>/g, ",");
-    return text.length > 15 ? text.substr(0, 10) + "..." : text
-  }
-  },
-  {title: '本科专业', dataIndex: 'undergradPro', key: 'undergradPro'},
-  {title: '备注', dataIndex: 'remark', key: 'remark'},
+  {title: '类别', dataIndex: 'name', key: 'name'},
 ]
 
 class Category extends Component {
 
+  handleRefresh = (params) => {
+    this.setState({table_loading: true});
+    loadDataProfessionCategoryDataSet(params).then(data => {
+      this.setState({dataSet: data.data.dataSet.rows, table_total: data.data.dataSet.total, table_loading: false})
+    })
+  }
+  onChangeTablePage = (currentPage) => {
+    this.setState({table_loading: true, table_cur_page: currentPage});
+    let searchForm = this.state.search_form;
+    searchForm['page'] = currentPage;
+    searchForm['status'] = (this.state.recycle_data ? 2 : 1);
+    this.handleRefresh(searchForm)
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      dataSet: [],
       table_loading: false,
-      selectedRowKeys: [],
+      dataSet: [],
+      search_form: {},
       table_cur_page: 1,
       table_total: 0,
-      search_form: {},
-      update_display: false,
-      update_data: {},
-      detail_display: false,
-      detail_data: {},
-      recycle_data: false,
     };
   }
 
-  render() {
-    const {table_loading, selectedRowKeys, table_cur_page, table_total} = this.state;
+  componentDidMount() {
+    this.handleRefresh({status: this.state.recycle_data ? 2 : 1});
+  }
 
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
+  render() {
+    const {table_loading, table_cur_page, table_total} = this.state;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: {span: 24},
+        sm: {span: 4},
+      },
+      wrapperCol: {
+        xs: {span: 24},
+        sm: {span: 18},
+      },
+      style: {
+        marginBottom: '8px'
+      }
     };
 
     return (
-      <div style={{backgroundColor: '#fff', padding: '10px'}}>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="专业列表" key="1">
-            <Filter
-              doSearch={this.handleSearch}
-              doRefresh={() => this.handleRefresh({page: this.state.table_cur_page, status: '1'})}
-              doRecycle={() => {
-                this.handleRefresh({status: this.state.recycle_data ? 1 : 2});
-                this.setState({recycle_data: !this.state.recycle_data});
-              }}
-              doDelete={this.handleDelete}
-              doUpdate={this.handleUpdate}
-            />
-            <Table
-              dataSource={this.state.dataSet}
-              columns={table_columns}
-              pagination={false}
-              rowKey={record => record.id + ''}
-              loading={table_loading}
-              bordered
-              rowSelection={rowSelection}
-              onRowClick={this.handleShowDetail}
-            />
-            <Pagination style={{marginTop: '10px'}} showQuickJumper defaultCurrent={1} current={table_cur_page}
-                        defaultPageSize={20} total={table_total} onChange={this.onChangeTablePage}/>,
-          </TabPane>
-          <TabPane tab="新建" key="2">
-            <New/>
-          </TabPane>
-          <TabPane tab="学科管理" key="3">
-            <Subject/>
-          </TabPane>
-          <TabPane tab="门类管理" key="4">
-            <Category/>
-          </TabPane>
-        </Tabs>
-        <Update show={this.state.update_display} data={this.state.update_data}
-                onCancel={() => this.setState({update_display: false})}/>
-        <Detail show={this.state.detail_display} data={this.state.detail_data}
-                onCancel={() => this.setState({detail_display: false})}/>
+      <div style={{backgroundColor: '#fff', padding: '10px'}} width={'50%'}>
+        <Table dataSource={this.state.dataSet} columns={table_columns} pagination={false}
+               rowKey={record => record.id + ''} loading={table_loading}/>
+        <Pagination style={{marginTop: '10px'}} defaultCurrent={1} current={table_cur_page} defaultPageSize={20}
+                    total={table_total} onChange={this.onChangeTablePage}/>
       </div>
     );
   }
