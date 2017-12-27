1fac79dc7bc5056c8133305db0c3b5b5ee44bca8
houser <houserqu@qq.com>
2017-12-27 14:37:10 +0800

    更换为  https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const siteName = '经英教育';
 
-export const API_DOMAIN = 'http://jywxsitex.sinogood.com/';
+export const API_DOMAIN = 'https://jywxsitex.sinogood.com/';
 // export const API_DOMAIN = 'http://106.14.112.47/';
 // export const API_DOMAIN = '//172.25.253.159:81/';
 
